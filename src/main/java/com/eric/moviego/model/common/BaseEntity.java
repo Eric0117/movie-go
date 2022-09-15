a5fa62377ccb0711fb09dd148c81a6144bc361e3
Eric <ksh117@kakao.com>
2022-09-16 00:01:59 +0900

    feat : BaseEntity 추가

---
+++ src/main/java/com/eric/moviego/model/common/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.eric.moviego.model.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/**
+ * @Author Eric
+ * @Description
+ * @Since 22. 9. 15.
+ **/
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "modified_date")
+    private LocalDateTime modifiedDate;
+}
